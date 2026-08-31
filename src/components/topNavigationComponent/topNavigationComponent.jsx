bec98a410b1e7865ffcf84ff049347489aad0723
miguel <seguroluismiguel@gmail.com>
2026-08-30 21:31:20 -0500

    refactor(topnavigationcomponent): remove excessive comments

--- src/components/topNavigationComponent/topNavigationComponent.jsx
+++ src/components/topNavigationComponent/topNavigationComponent.jsx
@@ -1,33 +1,25 @@
-import './topNavigationComponent.css' // importa los estilos de la barra
-import { FiSearch, FiBell } from 'react-icons/fi' // Fi = Feather Icons: lupa y campana
-import { HiOutlineBuildingOffice2 } from 'react-icons/hi2' // icono de edificio para organización
+import './topNavigationComponent.css'
+import { FiSearch, FiBell } from 'react-icons/fi'
+import { HiOutlineBuildingOffice2 } from 'react-icons/hi2'
 
-// Componente de navegación superior 
 function TopNavigationComponent({ organizationName = "Altos del Parque" }) {
   return (
-    <div className="topBar"> {/* contenedor flex principal alineado a la derecha */}
-      
-      {/* Caja de búsqueda global */}
+    <div className="topBar">
       <div className="searchBox">
-        <FiSearch /> {/* icono lupa */}
-        <input placeholder="Buscar apartamento, residente, pago..." /> {/* campo de búsqueda */}
+        <FiSearch />
+        <input type="search" placeholder="Buscar apartamento, residente, pago..." />
       </div>
-
-      {/* Botón de notificaciones con badge */}
       <button className="notifBtn">
-        <FiBell /> {/* icono campana */}
-        <span className="badge">3</span> {/* bolita roja con contador */}
+        <FiBell />
+        <span className="badge">3</span>
       </button>
-
-      {/* Selector de organización/edificio */}
       <div className="orgSelector">
-        <HiOutlineBuildingOffice2 /> {/* icono edificio */}
-        <span>{organizationName}</span> {/* nombre dinámico, no hardcodeado */}
-        <span>▼</span> {/* flecha dropdown */}
+        <HiOutlineBuildingOffice2 />
+        <span>{organizationName}</span>
+        <span>▼</span>
       </div>
-
     </div>
   )
 }
 
-export default TopNavigationComponent // lo exporta para usar en App.jsx
+export default TopNavigationComponent
