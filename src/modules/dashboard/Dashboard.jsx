85c15a3ba870bdf49d7347f381d42dfe7a7e7c2f
juan david <arangojuandavid438@gmail.com>
2026-09-01 14:22:24 -0500

    feat(tablecomponent): add reusable table component

--- src/modules/dashboard/Dashboard.jsx
+++ src/modules/dashboard/Dashboard.jsx
@@ -1,12 +1,15 @@
+import PaymentsTableExample from './components/TableComponent/PaymentsTableExample'
 import './Dashboard.css'
 
 import TopNavigationComponent from './components/topNavigationComponent/topNavigationComponent'
 import InformationCardsComponent from './components/InformationCardsComponent/InformationCardsComponent'
 function DashBoard() {
+
   return (
     <>
       <TopNavigationComponent organizationName="Altos del Parque" />
       <InformationCardsComponent/>
+      <PaymentsTableExample/>
     </>
   )
 
