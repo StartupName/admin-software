d4c1348b1456c3557b97f41ce0543837c1805406
juan david <arangojuandavid438@gmail.com>
2026-09-05 03:27:03 -0500

    feat(reuse-component-top-navegation): reusable component

--- src/modules/apartments/Apartments.jsx
+++ src/modules/apartments/Apartments.jsx
@@ -1,10 +1,16 @@
 import './Apartments.css';
+import TopNavigationComponent from '../dashboard/components/topNavigationComponent/topNavigationComponent';
+import topNavData from './components/TopNavigation/example_data.json';
 import NavigationComponent from './components/NavigationComponent/NavigationComponent';
 import SummaryComponent from './components/SummaryComponent/SummaryComponent';
 
 function Apartments() {
   return (
     <>
+      <TopNavigationComponent
+        organizationName={topNavData.organizationName}
+        notificationCount={topNavData.notificationCount}
+      />
       <SummaryComponent values={[120, 78, 28, 14, 92]} />
       <NavigationComponent />
     </>
