7dd58e47f571a9c4934017efc219fae42f475f58
José Martinez <martinez.jose2927@gmail.com>
2026-09-05 14:11:18 -0500

    feat(modules-expenses): Adding structure for the new module.
    - Adding a module to the routing system.

---
+++ src/modules/expenses/expenses.jsx
@@ -0,0 +1,12 @@
+import './expenses.css'
+
+export default function Expenses(){
+    return (
+        <>
+            <div className="expenses-table">
+                {/* Put the table componente here */}
+                <h1>Gastos</h1>
+            </div>
+        </>
+    )
+}
